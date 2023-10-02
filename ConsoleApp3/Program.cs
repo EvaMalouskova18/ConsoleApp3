1999ac764ce0c3ae6e82d100ab2122ef15b005fd
@EvaMalouskova18 <evamalouskova@gmail.com>
2023-10-02 14:43:55 +0200

    test

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worlod eva,ddddd!");
+﻿// See https://aka.ms/new-console-template for moreano information
+Console.WriteLine("Hello, Worlod eva,ddddd!sssssss");
