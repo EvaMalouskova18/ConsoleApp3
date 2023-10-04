07b8e932a99c2ab20eea48a3fa66e7ddf81ab202
@EvaMalouskova18 <evamalouskova@gmail.com>
2023-10-04 10:40:02 +0200

    #zkouska

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for moreano information
-Console.WriteLine("Hello, Worlod eva,ddddd!sssssss");
+Console.WriteLine("Hello, World!");
 Console.WriteLine("ahoj, dobre?");
